5faa09faf36ca36b8e5d2e9d1c61e485381a7824
AthiraAnoop <127675396+AthiraAnoop@users.noreply.github.com>
2024-01-30 19:24:04 +0530

    Ticket Tracker

---
+++ TicketCounters/src/main/java/com/gl/dao/TicketDAOImpl.java
@@ -0,0 +1,76 @@
+package com.gl.dao;
+
+import java.util.List;
+
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
+import javax.persistence.criteria.Path;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.gl.entity.TicketModel;
+
+@Repository
+public class TicketDAOImpl implements TicketDAO {
+	
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	@Override
+	public List<TicketModel> getAllTickets() {
+		
+		Session session=sessionFactory.getCurrentSession();
+		CriteriaBuilder cb=session.getCriteriaBuilder();
+		CriteriaQuery <TicketModel> cq=cb.createQuery(TicketModel.class);
+		Root <TicketModel> root=cq.from(TicketModel.class);
+		cq.select(root);
+		Query query=session.createQuery(cq);
+		return query.getResultList();
+	}
+
+	@Override
+	public void saveTicket(TicketModel theTicket) {
+		Session currentSession=sessionFactory.getCurrentSession();
+		currentSession.saveOrUpdate(theTicket);
+			
+	}
+
+	@Override
+	public TicketModel getTicket(int theId) {
+		Session currentSession=sessionFactory.getCurrentSession();
+		TicketModel fetchTicket=currentSession.get(TicketModel.class,theId);
+		return fetchTicket;
+	}
+
+	@Override
+	public void deleteTicket(int theId) {
+		Session currentSession=sessionFactory.getCurrentSession();
+		TicketModel ticket =currentSession.byId(TicketModel.class).load(theId);
+		currentSession.delete(ticket);
+		
+	}
+
+	@Override
+	public List<TicketModel> getSearchWithTitle(String title) {
+		
+		Session session = sessionFactory.getCurrentSession();
+	    CriteriaBuilder cb = session.getCriteriaBuilder();
+	    CriteriaQuery<TicketModel> cq = cb.createQuery(TicketModel.class);
+	    Root<TicketModel> root = cq.from(TicketModel.class);
+	    Path<String> titlePath = root.get("title");
+	    Expression<String> titleExpression = cb.lower(titlePath);
+	    String searchTerm = "%" + title.toLowerCase() + "%";
+	    cq.select(root).where(cb.like(titleExpression, searchTerm));    
+	    Query query = session.createQuery(cq);
+	    return query.getResultList();
+	}
+	
+	
+
+}
---
+++ ticketsystem.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 30, 2024 at 02:21 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ticketsystem`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket`
+--
+
+CREATE TABLE `ticket` (
+  `id` int(20) NOT NULL,
+  `title` varchar(55) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `content` varchar(255) DEFAULT NULL,
+  `createdon` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `ticket`
+--
+
+INSERT INTO `ticket` (`id`, `title`, `description`, `content`, `createdon`) VALUES
+(3, 'First', 'First Description', '<p>dgfhdsghfdghgh</p>\r\n', '2024-01-30 07:51:38');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `ticket`
+--
+ALTER TABLE `ticket`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `ticket`
+--
+ALTER TABLE `ticket`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
